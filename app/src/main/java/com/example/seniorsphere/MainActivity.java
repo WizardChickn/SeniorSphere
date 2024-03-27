d6f7705c3cd9a34b101cf14c6d5782b4cc55bcb0
eddiegoodwin <edwardthomasgoodwin@gmail.com>
2024-03-27 14:09:23 -0400

    New stuff

--- app/src/main/java/com/example/seniorsphere/MainActivity.java
+++ app/src/main/java/com/example/seniorsphere/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.seniorsphere;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    String name;
+    EditText nameInput;
+    Button submitButton;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nameInput = (EditText) findViewById(R.id.nameInput);
+
+        submitButton = (Button) findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                name = nameInput.getText().toString();
+                showToast(name);
+            }
+        });
+    }
+
+    private void showToast(String name) {
+        Toast.makeText(MainActivity.this, name, Toast.LENGTH_SHORT).show();
+    }
+    public String getName(){
+        return name;
     }
 }
