b248b630d63607065606b84b3445e30d1ccc6f40
WizardChickn <164069315+WizardChickn@users.noreply.github.com>
2024-05-09 21:48:03 -0400

    Update Skill.java

--- app/src/main/java/com/example/seniorsphere/Skill.java
+++ app/src/main/java/com/example/seniorsphere/Skill.java
@@ -1,15 +1,23 @@
 package com.example.seniorsphere;
-//object that contains the data for each skill
-//
+/*
+* stores the data of each skill
+/
 public class Skill{
+    
     String skill;
+    
     double hours;
-
+    
+    /*
+    *
+    /
     public Skill(String name){
         skill  = name;
         hours = 0;
     }
-
+    /*
+    *
+    /
     public String getSkill() {
         return skill;
     }
