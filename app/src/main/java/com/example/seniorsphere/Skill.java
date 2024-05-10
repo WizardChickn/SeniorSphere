7aad677a3f612572c986f045c8caaade4d5a686e
eddiegoodwin <edwardthomasgoodwin@gmail.com>
2024-05-09 21:52:03 -0400

    documentation

--- app/src/main/java/com/example/seniorsphere/Skill.java
+++ app/src/main/java/com/example/seniorsphere/Skill.java
@@ -1,7 +1,7 @@
 package com.example.seniorsphere;
 /*
 * stores the data of each skill
-/
+*/
 public class Skill{
     
     String skill;
@@ -9,15 +9,17 @@ public class Skill{
     double hours;
     
     /*
-    *
-    /
+    * creates an object that stores data on the skill
+    * @Param name
+    */
     public Skill(String name){
         skill  = name;
         hours = 0;
     }
     /*
-    *
-    /
+    *returns the name of the skill
+    * @Return the name of the skill
+    */
     public String getSkill() {
         return skill;
     }
--- app/src/main/java/com/example/seniorsphere/StatsData.java
+++ app/src/main/java/com/example/seniorsphere/StatsData.java
@@ -3,12 +3,19 @@ package com.example.seniorsphere;
 
 
 
-//contains data on the user's activity in the app
+/*
+* contains data on the user's activity in the app
+ */
 public class StatsData {
 
     public static Skill Skill1 = new Skill("Strategy");
     public static Skill Skill2 = new Skill("Logic");
     public static Skill Skill3 = new Skill("Patterns");
+    /*
+    *returns the name of the skill for a skill object
+    *@Param name of the skill object
+    *@Return name of the skill
+     */
     public String getSkill(Skill skill) {
         return skill.getSkill();
     }
